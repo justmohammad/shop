aed2c0f43190d293dc4cfe2ab5d9f0790fc1a46d
Mohammad <justmohammad403@gmail.com>
2022-02-16 23:53:17 +0330

    Complete Pagination

--- src/Component/SingleProduct/SingleProduct.js
+++ src/Component/SingleProduct/SingleProduct.js
@@ -1,23 +1,29 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Button, Col} from "react-bootstrap";
 import './SingleProduct.css'
+import axios from "axios";
 
-const SingleProduct = ({product}) => {
+const SingleProduct = () => {
 
-return (
-    <>
-        {
-            product.slice(0, 6).map(value =>
-                <Col md={"4"} className="product">
-                    <img src={value.img} alt=""/>
-                    <p>{value.title}</p>
-                    <div className="price">{`$${value.price}`}</div>
-                    <Button>Add To Cart</Button>
-                </Col>
-            )
-        }
-    </>
-);
+    const [data, setData] = useState([])
+
+    useEffect(() => {
+        axios.get('http://localhost:4000/product').then(response => setData(response.data))
+    }, [data])
+    return (
+        <>
+            {
+                data.slice(0,6).map(value =>
+                    <Col md={"4"} className="product">
+                        <img src={value.img} alt=""/>
+                        <p>{value.title}</p>
+                        <div className="price">{value.price}</div>
+                        <Button>Add To Cart</Button>
+                    </Col>
+                )
+            }
+        </>
+    );
 
 }
 
