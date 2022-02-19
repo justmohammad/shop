b85d464757f98ec1ef74d5f445f21b9df44168fe
Mohammad <justmohammad403@gmail.com>
2022-02-19 18:45:18 +0330

    Start React-router-dom

--- src/Component/App/App.js
+++ src/Component/App/App.js
@@ -4,16 +4,27 @@ import Header from "../../Component/Header/Header";
 import Blog from "../../Component/Blog/Blog";
 import Footer from "../../Component/Footer/Footer";
 import ContentShop from "../../Component/ContentShop/ContentShop";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
+import Details from "../Details/Details";
 
 const App = () => {
 
     return (
-        <div className="App">
-            <Header/>
-            <ContentShop/>
-            <Blog/>
-            <Footer/>
-        </div>
+        <BrowserRouter>
+            <Switch>
+                <Route path={"/details"}>
+                    <Details/>
+                </Route>
+                <Route path={"/"} exact>
+                    <div className="App">
+                        <Header/>
+                        <ContentShop/>
+                        <Blog/>
+                        <Footer/>
+                    </div>
+                </Route>
+            </Switch>
+        </BrowserRouter>
     );
 }
 
---
+++ src/Component/Details/Details.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Details = () => {
+    return (
+        <div>
+            salam
+        </div>
+    );
+};
+
+export default Details;
