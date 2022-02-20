a7564e34e3469f449366ecf03575b60d2897a4df
Mohammad <justmohammad403@gmail.com>
2022-02-20 20:24:09 +0330

    debug and remove conflict

--- src/Component/App/App.js
+++ src/Component/App/App.js
@@ -1,42 +1,15 @@
 import React,{Suspense} from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-<<<<<<< HEAD
-import Header from "../../Component/Header/Header";
-import Blog from "../../Component/Blog/Blog";
-import Footer from "../../Component/Footer/Footer";
-import ContentShop from "../../Component/ContentShop/ContentShop";
-import {BrowserRouter, Route, Switch} from "react-router-dom";
-import Details from "../Details/Details";
-=======
 import {BrowserRouter, Route, Switch} from "react-router-dom";
 import Layout from "../Layout/Layout";
 import PageShop from "../PageShop/PageShop";
 import Page404 from "../Page404/Page404";
 import DetailProduct from "../DetailProduct/DetailProduct";
->>>>>>> front-end
 
 const App = () => {
 
     return (
-<<<<<<< HEAD
-        <BrowserRouter>
-            <Switch>
-                <Route path={"/details"}>
-                    <Details/>
-                </Route>
-                <Route path={"/"} exact>
-                    <div className="App">
-                        <Header/>
-                        <ContentShop/>
-                        <Blog/>
-                        <Footer/>
-                    </div>
-                </Route>
-            </Switch>
-        </BrowserRouter>
-=======
-
         <div className="App">
             <Suspense fallback={null}>
                 <BrowserRouter>
@@ -56,7 +29,6 @@ const App = () => {
                 </BrowserRouter>
             </Suspense>
         </div>
->>>>>>> front-end
     );
 }
 
